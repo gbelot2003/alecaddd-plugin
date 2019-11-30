c7bc96fd5fc7eb6f0cd602145437ffe243f5a298
Gerardo Belot <gerardo.belot@gmail.com>
2019-11-29 23:37:29 -0600

    First commit

---
+++ alecaddd-plugin.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @package AlecadddPlugin
+ */
+
+/*
+Plugin Name: Alecaddd Plugin
+Plugin URI: http://alecaddd.com/plugin
+Description: Primera prueba de creación de un plugin
+Version: 0.0.1
+Author: Gerardo Belot
+Author URI: http://gerardobelot.me
+License: GPLv2 or later
+Text Domain: alecaddd-plugin
+*/
+
+/**
+ * AlecadddPlugin
+ *
+ * Plugin Name: Classic Editor
+ * Plugin URI:  https://wordpress.org/plugins/classic-editor/
+ * Description: Enables the WordPress classic editor and the old-style Edit Post screen with TinyMCE, Meta Boxes, etc. Supports the older plugins that extend this screen.
+ * Version:     1.5
+ * Author:      WordPress Contributors
+ * Author URI:  https://github.com/WordPress/classic-editor/
+ * License:     GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Text Domain: classic-editor
+ * Domain Path: /languages
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License version 2, as published by the Free Software Foundation. You may NOT assume
+ * that you can use any other version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+ if (! defined('ABSPATH')){
+     die;
+ }
